315ed8184c5566c00194239e533387bc767c02dc
MingTao <MingTao@DESKTOP-OUH8J4U>
2019-07-05 08:31:20 +0800

    上班

--- LYMG.SqlDB/DbClient.cs
+++ LYMG.SqlDB/DbClient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ namespace LYMG.SqlDB
     public class DbClient : IDisposable
     {
         public readonly DbProvider Provider;
+        public IDbConnection Connection;
 
         public DbClient(DbProvider provider)
         {
